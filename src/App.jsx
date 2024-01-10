d6f519ee2cabdb8a5e86a7a326c57af03fbea4ab
Chandrakant Kushwah <chandrakantkushwah@Chandrakants-MacBook-Air.local>
2024-01-10 13:41:00 +0530

    landing changed

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,42 @@ import Artist from "./page/Artist";
 import Home from "./page/Home";
 import Navigation from "./component/layout/Navigation";
 
+import { useState } from "react";
+
 // Main App component representing the entire application
 const App = () => {
+  const [toggle, setToggle] = useState(true);
   return (
-    <div className="h-screen w-screen overflow-hidden flex flex-col ">
+    <div className="h-screen w-screen overflow-hidden flex flex-col relative ">
+      {toggle && (
+        <div className="absolute w-full h-full bg-white z-50 flex items-center justify-center">
+          <div className="w-72">
+            <h3 className="text-lg font-medium text-center">
+              SarvaLabs Assessment
+            </h3>
+            <div className="flex gap-2 items-center mt-2">
+              <div
+                onClick={() => {
+                  setToggle(!toggle);
+                }}
+                className="cursor-pointer transition duration-300 flex-1 py-2 px-5 text-xs font-medium bg-black hover:bg-gray-800 text-white rounded flex gap-1 items-center justify-center"
+              >
+                Demo
+              </div>
+
+              <a
+                href="https://github.com/03brainy-clicks/sarvalabs/"
+                target="_black"
+                className="block w-1/2"
+              >
+                <div className="w-full transition duration-300 cursor-pointer flex-1 py-2 px-5 text-xs font-medium border border-black text-black  rounded flex gap-1 items-center justify-center">
+                  Repo
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      )}
       <div className="h-full w-full flex overflow-hidden">
         {/* Left sidebar */}
         <div className="min-w-44 bg-primary h-full px-2 py-4 md:flex flex-col hidden gap-4 ">
